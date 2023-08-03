38cf9d85386d3040f7a63a1519732cb7daf41125
Nathan Fritzler <nfblaster@live.com>
2023-08-02 21:21:38 -0600

    Add todo for neo-tree v3 update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -721,7 +721,7 @@ local lazy_plugins = {
 	-- Even nicer file management
 	{
 		'nvim-neo-tree/neo-tree.nvim',
-		branch = "v2.x",
+		branch = "v2.x", -- TODO migrate to v3
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
 		config = function ()
 			vim.g.neo_tree_remove_legacy_commands = 1
